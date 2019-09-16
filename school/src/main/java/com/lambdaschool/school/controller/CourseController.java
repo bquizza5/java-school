49dd400b2e9df491ef33e84495641dbc5f9b3ff4
bquizza <blaine555@gmail.com>
2019-09-16 17:11:13 -0600

    mvp

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -7,27 +7,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Date;
 
 @RestController
 @RequestMapping(value = "/courses")
 public class CourseController
 {
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
     @Autowired
     private CourseService courseService;
 
     @GetMapping(value = "/courses", produces = {"application/json"})
-    public ResponseEntity<?> listAllCourses()
+    public ResponseEntity<?> listAllCourses(HttpServletRequest request)
     {
+        logger.trace(request.getRequestURL() + " accessed at trace level " + new Date());
         ArrayList<Course> myCourses = courseService.findAll();
         return new ResponseEntity<>(myCourses, HttpStatus.OK);
     }
 
     @GetMapping(value = "/studcount", produces = {"application/json"})
-    public ResponseEntity<?> getCountStudentsInCourses()
+    public ResponseEntity<?> getCountStudentsInCourses(HttpServletRequest request)
     {
-        return new ResponseEntity<>(courseService.getCountStudentsInCourse(), HttpStatus.OK);
+        logger.trace(request.getRequestURL() + " accessed at trace level " + new Date());
+        ArrayList<CountStudentsInCourses> count = courseService.getCountStudentsInCourse();
+        return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
     @DeleteMapping("/courses/{courseid}")
